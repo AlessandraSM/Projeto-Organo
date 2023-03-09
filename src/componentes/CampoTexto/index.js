2afedc490b3a51dc7c89a7e8b85f0019534ba987
Alessandra-Marmiroli <alessandramarmiroli@gmail.com>
2023-03-09 15:59:17 -0300

    States no Formulario

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,12 +1,29 @@
-import './CampoTexto.css'
+import { useState } from "react";
+import "./CampoTexto.css";
 
-function CampoTexto(props){
-    return(
-        <div className="campo-texto">
-            <label>{props.label}</label>
-            <input required={props.obrigatorio}placeholder={props.placeholder}/>
-        </div>
-        
-    )
+function CampoTexto(props) {
+  const placeholderModificada = `${props.placeholder}...`;
+
+  // let valor = 'Alessandra Marmiroli'
+
+
+  //vai retornar o valor em si + o set, que é pra quando eu quiser alterar, essa função set será chamada
+  // não posso alterar o valor direto por atribuição, é preciso passar pelo set
+
+  const aoDigitado = (evento) => {
+   props.aoAlterado(evento.target.value)
+  };
+
+  return (
+    <div className="campo-texto">
+      <label>{props.label}</label>
+      <input
+        value={props.value}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        placeholder={props.placeholder}
+      />
+    </div>
+  );
 }
-export default CampoTexto
+export default CampoTexto;
